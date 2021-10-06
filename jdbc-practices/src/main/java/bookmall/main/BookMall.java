6626000a7090cdf7ea9ba48a3b9db3e1a49d42dd
soottoppi <duwjs1103@naver.com>
2021-10-06 16:03:23 +0900

    DAO 패턴 연습 #3

---
+++ jdbc-practices/src/main/java/bookmall/main/BookMall.java
@@ -0,0 +1,13 @@
+package bookmall.main;
+
+public class BookMall {
+	public static void main(String[] args) {
+//		new CategoryDao().insert(vo);
+//		new CategoryDao().insert(vo);
+//		new CategoryDao().insert(vo);
+//		
+//		new CategoryDao().findAll();
+		
+		System.out.println("=========회원 리스트==========");
+	}
+}
