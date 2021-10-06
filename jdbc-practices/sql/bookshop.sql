7f0672c167a2baa04d45b42a0298f11085fedc38
soottoppi <duwjs1103@naver.com>
2021-10-06 20:30:59 +0900

    bookshop sql

--- jdbc-practices/sql/bookshop.sql
+++ jdbc-practices/sql/bookshop.sql
@@ -4,7 +4,7 @@ desc author;
 
 -- insert author
 insert into author values(null, '스테파니메이어');
-select * from author;
+select no, name from author;
 -- insert book
 insert into book values(null, '이클립스', 1, '대여가능');
 select * from book;
@@ -19,3 +19,5 @@ update book
 	set status = '대여중'
 where no = 1;
 select * from book;
+
+insert into book values(nauthorull, 'gk', 2, 'ask')
