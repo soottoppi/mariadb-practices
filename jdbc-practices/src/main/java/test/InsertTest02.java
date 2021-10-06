cbd9cbc73398feec04efe4227730702c5a3540ea
soottoppi <duwjs1103@naver.com>
2021-10-06 10:20:23 +0900

    PreparedStatement #1

---
+++ jdbc-practices/src/main/java/test/InsertTest02.java
@@ -0,0 +1,64 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InsertTest02 {
+
+	public static void main(String[] args) {
+		insert("영업");
+		insert("개발");
+		insert("기획");
+
+	}
+
+	private static boolean insert(String name) {
+		boolean result = false;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			// 1. JDBC Driver 로딩
+			Class.forName("org.mariadb.jdbc.Driver");
+
+			// 2. 연결하기
+			String url = "jdbc:mysql://127.0.0.1:3306/employees?charset=utf8";
+			conn = DriverManager.getConnection(url, "hr", "hr");
+
+			// 3. SQL문 준비
+			String sql = "insert into dept values(null, ?)";
+			pstmt = conn.prepareStatement(sql);
+
+			// 4. 바인딩(binding)
+			pstmt.setString(1, name);
+			
+			// 5. SQL 실행(하기전에 워크벤치에서 연습)
+			int count = pstmt.executeUpdate();
+			if (count == 1) {
+				result = true;
+			}
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패");
+		} catch (SQLException e) {
+			System.out.println("error : " + e);
+		} finally {
+			// clean up
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+
+	}
+
+}
